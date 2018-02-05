7b7ab64106ef8509c95c81bc7f777acbac4dfdd6
xizonghu <xizonghu@163.com>
2018-02-05 14:31:06 +0800

    update

--- java/TreeTest.java
+++ java/TreeTest.java
@@ -12,12 +12,29 @@ public class TreeTest {
 			this.right = right;
 		}
 	}
-	public static boolean isValidBST(TreeNode node) {
-		if (node == null) return true;
 
-		if (node.left != null && node.left.value >= node.value)  return false;
-		if (node.right != null && node.right.value <= node.value)  return false;
-		return isValidBST(node.left) && isValidBST(node.right);
+	public static int getMaxValue(TreeNode node) {
+		if (node == null) return 0;
+		int max;
+		max = Integer.max(node.value, getMaxValue(node.left));
+		max = Integer.max(max, getMaxValue(node.right));
+		return max;
+	}
+
+	public static int getMinValue(TreeNode node) {
+		if (node == null) return 0;
+		int min;
+		min = Integer.min(node.value, getMaxValue(node.left));
+		min = Integer.min(min, getMaxValue(node.right));
+		return min;
+	}
+
+	public static boolean isValidBST(TreeNode root) {
+		if (root == null) return true;
+
+		if (root.left != null && getMaxValue(root.left) >= root.value)  return false;
+		if (root.right != null && getMinValue(root.right) <= root.value)  return false;
+		return isValidBST(root.left) && isValidBST(root.right);
 	}
 
 	public static void main(String[] args) {
@@ -25,8 +42,8 @@ public class TreeTest {
 		TreeNode two = new TreeNode(2, one, null);
 		TreeNode four = new TreeNode(4, null, null);
 		TreeNode three = new TreeNode(3, two, four);
-		TreeNode seven = new TreeNode(7, null, null);
-		TreeNode nine = new TreeNode(6, null, null);
+		TreeNode seven = new TreeNode(4, null, null);
+		TreeNode nine = new TreeNode(9, null, null);
 		TreeNode eight = new TreeNode(8, seven, nine);
 		TreeNode five = new TreeNode(5, three, eight);
 
